65bf34b98e39840c397fa22dd55b0b2cd03342e2
Fabio-F16 <fipsfabio95@gmail.com>
2022-07-19 22:09:25 +0200

    Modello di supporto

---
+++ Models/PizzaCategories.cs
@@ -0,0 +1,8 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaCategories
+    {
+        public Pizza Pizza { get; set; }
+        public List<Categoria>? Categorias { get; set; }
+    }
+}
