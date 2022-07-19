ad47e3ac584b9c073a6679d71593572ae7cc0f91
Fabio-F16 <fipsfabio95@gmail.com>
2022-07-19 22:08:21 +0200

    Aggiunte a db le categorie

--- Database/PizzaContext.cs
+++ Database/PizzaContext.cs
@@ -7,6 +7,7 @@ namespace la_mia_pizzeria_static.Database
     {
         public DbSet<Pizza> Pizze { get; set; }
         public DbSet<Ingrediente> ListaIngredienti { get; set; }
+        public DbSet<Categoria> Categorie { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
         {
